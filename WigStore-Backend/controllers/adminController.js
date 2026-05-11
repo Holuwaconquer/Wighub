b7fefca542c34a0b00ba676af7ceb960b4071ea7
Fakorede Peter Okikiola <holuwaconquer488@gmail.com>
2026-05-11 20:49:28 +0100

    change the casing for user model reference

--- WigStore-Backend/controllers/adminController.js
+++ WigStore-Backend/controllers/adminController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Product = require('../models/Product');
 const Order = require('../models/Order');
 const Coupon = require('../models/Coupon');
--- WigStore-Backend/controllers/userController.js
+++ WigStore-Backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Product = require('../models/Product');
 
 // @desc    Get user wishlist
